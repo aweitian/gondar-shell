e699889e276a162b4d058e4a95a38f6c5160a2e5
aw <1541998@qq.com>
2026-05-28 16:32:21 +0800

    init - 环境搭建,打包完成

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="container">
+    <a-button type="primary" @click="showModal">点击弹出 Hello World</a-button>
+    <a-modal v-model:open="isModalOpen" title="提示">
+      <p>Hello World</p>
+    </a-modal>
+  </div>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+
+const isModalOpen = ref(false)
+
+const showModal = () => {
+  isModalOpen.value = true
+}
+</script>
+
+<style scoped>
+.container {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+}
+</style>
